7e2395973fbe6d20c35bb372cc1253368838bace
Angela Meyer <angela.td.meyer@gmail.com>
2012-12-02 16:57:09 -0800

    Literally changed anything sampleapp to fennec

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '21e2e170e7c2840b67481752c61dfc74af682d4ee40ceacaa6a51e2e0acc788c89e2fb24ed17e61772380750a76f34c8cd7a07d85e7561c0b1ae74fe72bb979d'
+Fennec::Application.config.secret_token = '21e2e170e7c2840b67481752c61dfc74af682d4ee40ceacaa6a51e2e0acc788c89e2fb24ed17e61772380750a76f34c8cd7a07d85e7561c0b1ae74fe72bb979d'
