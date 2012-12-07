39acd490bc23500d2f471e77a29b3f92ee3386f0
ChelseyFrank <chelsey@zaarly.com>
2012-12-07 11:51:09 -0800

    Added a Homepage, Topic1, and Topic 2 page. Updated application helper and application.html.erb

---
+++ spec/helpers/section_pages_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the SectionPagesHelper. For example:
+#
+# describe SectionPagesHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe SectionPagesHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
