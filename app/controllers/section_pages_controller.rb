8b5cac5595c188108f3b4dda006ccc4550da4a31
ChelseyFrank <chelsey@zaarly.com>
2012-12-20 18:28:33 -0800

    Added save_aeo to controller

--- app/controllers/section_pages_controller.rb
+++ app/controllers/section_pages_controller.rb
@@ -17,6 +17,17 @@ cclass SectionPagesController < ApplicationController
   	redirect "/checklist?checklist_id=#{c.id}"
   end
 
+  def save_aeo
+  	aeo_id = params['id']
+  	aeo_state = params['state']
+
+  	aeo = AEOModel.find_by_id(aeo_id)
+  	aeo.state = aeo_state
+  	aeo.save()
+
+  	redirect "/section"
+  end
+  
   def topic2
   end
 
